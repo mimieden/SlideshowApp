979033416290d78efc62f3c5414413144685bdfd
mimieden <mimieden525@gmail.com>
2017-06-17 01:40:37 +0900

    0.04 タイトル切り替えの判定条件をカレントタイトルからステータスに変更①class下にステータス判定用の変数を宣言 (A_Push内の宣言ではダメ)②A_Push下でステータス判定/更新ロジックを追加　ステータスによるボタンタイトルの切り替えを行う

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    //再生停止ボタンのOutpet (0.02)
+    //再生停止ボタンのOutpet作成 (0.02)
     @IBOutlet weak var B_StartStop: UIButton!
+    
+    //ステータス判定用の変数 (0.03)
+    var V_CountUp: Int = 0             //カウントアップ
+    var V_Status: Int = 0              //ボタン押下時のステータス
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,19 +30,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    //再生停止ボタンのAction (0,03)
+    //再生停止ボタンのAction作成 (0.03)
     @IBAction func A_Push(_ sender: Any) {
         
-        //カレントタイトルを取得
-        let V_CurrentTitle:String = (sender as AnyObject).currentTitle!!
+        //カウントアップ変数よりステータス判断 (0.03)
+        V_Status = V_CountUp % 2          //カウントアップ変数から偶数/奇数を判断
+        V_CountUp += 1                    //ステータス判断したのでカウントアップ
         
-        //タイトルを切り替え
-        if V_CurrentTitle == "再生" {
-            B_StartStop.setTitle("停止", for: .normal)
-        } else {
-            B_StartStop.setTitle("再生", for: .normal)
+        //ステータスに応じてタイトルを切り替え (0.03)
+        if V_Status == 0 {                                //偶数 = 停止状態
+            B_StartStop.setTitle("停止", for: .normal)     //(再生して)停止ボタンを表示
+        } else {                                          //奇数 = 再生状態
+            B_StartStop.setTitle("再生", for: .normal)     //(停止して)再生ボタンを表示
         }
-        
     }
 }
 
