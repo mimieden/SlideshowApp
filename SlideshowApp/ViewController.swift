a2bffb9bb9b058b298602e01e09eeb05b4eecd3d
mimieden <mimieden525@gmail.com>
2017-06-17 01:02:39 +0900

    0.03 再生/停止ボタンのクリックでタイトル切り替えにロジック変更①viewDidLoadはクリック時に起動されないため、B_StartStopのAction A_Pushを作る②A_Push内でボタンタイトルの切りかを行う　カレントタイトルの取得　(参考:http://iphone-tora.sakura.ne.jp/uibutton.html UIButtonのプロパティ)　(参考:http://qiita.com/daisukeokaoss/items/7152913405cfda031052 ロジック)　カレントタイトルに応じて次にタイトルをセット③ボタンタイトルの初期値をviewDidLoadで行う

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -12,17 +12,13 @@ class ViewController: UIViewController {
 
     //再生停止ボタンのOutpet (0.02)
     @IBOutlet weak var B_StartStop: UIButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    
-    //再生停止ボタンの文字を設定する（通常時）(0.02)
+        
+        //再生停止ボタンの初期値(再生)を設定 (0.03)
         B_StartStop.setTitle("再生", for: .normal)
-        print(B_StartStop.titleLabel as Any)
-    //再生停止ボタンの文字を設定する（押下時）(0.02)
-        B_StartStop.setTitle("停止", for:.highlighted)
-        print(B_StartStop.titleLabel as Any)
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,6 +26,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //再生停止ボタンのAction (0,03)
+    @IBAction func A_Push(_ sender: Any) {
+        
+        //カレントタイトルを取得
+        let V_CurrentTitle:String = (sender as AnyObject).currentTitle!!
+        
+        //タイトルを切り替え
+        if V_CurrentTitle == "再生" {
+            B_StartStop.setTitle("停止", for: .normal)
+        } else {
+            B_StartStop.setTitle("再生", for: .normal)
+        }
+        
+    }
 }
 
