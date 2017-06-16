6d66249026bcf1481bd96bc10eaacda87a0270d8
mimieden <mimieden525@gmail.com>
2017-06-17 00:10:48 +0900

    0.02 通常時/押下時の再生/停止ボタンタイトルの切り替えを仮実装(参考:http://t-higashi.com/uibutton/)①再生停止ボタンをOutletとして設定（B_StartStop）②IdentityのRestoration IDにB_StartStopを設定③viewDidLoadで.B_StartStopに対し.setTitleを使って値をいれる　.normal 通常時　.higlighted 押下時 ※プロパティでの更新がうまくいかなかった(未調査)のでプログラムから更新

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //再生停止ボタンのOutpet (0.02)
+    @IBOutlet weak var B_StartStop: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    //再生停止ボタンの文字を設定する（通常時）(0.02)
+        B_StartStop.setTitle("再生", for: .normal)
+        print(B_StartStop.titleLabel as Any)
+    //再生停止ボタンの文字を設定する（押下時）(0.02)
+        B_StartStop.setTitle("停止", for:.highlighted)
+        print(B_StartStop.titleLabel as Any)
     }
 
     override func didReceiveMemoryWarning() {
