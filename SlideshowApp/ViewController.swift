64dfd470b2a25f5f2043527ced53e7edd3663195
mimieden <mimieden525@gmail.com>
2017-06-19 06:53:33 +0900

    1.06 変数/定数名の変更とコメントの更新

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,27 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+//==================================================
+// グローバル変数/定数
+//==================================================
     
-//--------------------------------------------------
-//  変数(Outlet) *Outlet作成&RestrationIDのセット
-//--------------------------------------------------
-    //スライドショーのOutlet作成
+//--Outlet(RestrationIDもセット!)---------------------
+    //スライドショーのOutlet
     @IBOutlet weak var IMG_Slideshow: UIImageView!
     
-    //再生停止ボタンのOutlet作成 
+    //再生停止ボタンのOutlet
     @IBOutlet weak var B_StartStop: UIButton!
     
-    //進むボタンのOutlet作成
+    //進むボタンのOutlet
     @IBOutlet weak var B_Next: UIButton!
     
-    //戻るボタンのOutlet作成
+    //戻るボタンのOutlet
     @IBOutlet weak var B_Previous: UIButton!
-    
+ 
 //--------------------------------------------------
-//  変数/定数
-//--------------------------------------------------
-    //画像の名称を配列に入れる (0.12)
-    let L_ImageName = [
+    //(画像ファイル名
+    let GL_ImageName = [
         "000.jpg",
         "001.JPG",
         "002.JPG",
@@ -49,84 +49,76 @@ class ViewController: UIViewController {
     ]
     
     //配列操作用のインデックス (0.12)
-    var V_Index: Int = 0
+    var GV_Index: Int = 0
     
     //配列から取得する画像名称 (0.12)
-    var V_PicName: String = ""
+    var GV_PicName: String = ""
     
     //スライドショーコントロールのためにタイマー用の変数を宣言 (0.20)
-    var V_Timer:Timer!
+    var GV_Timer:Timer!
 
-//--------------------------------------------------
+//==================================================
 //  関数(ライフサイクル)
-//--------------------------------------------------
-    //View読み込み後
+//==================================================
+//--View読み込み後------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        //再生停止ボタンの初期値(再生)を設定 (0.03)
+        //再生停止ボタンの初期値(再生)を設定
         B_StartStop.setTitle("再生", for: .normal)
         
-        //画像データ1枚目を表示 (0.14)
-        V_PicName = L_ImageName[V_Index]                 //インデックス(初期値)と同じインデックスの写真名を配列から変数に取得
-        IMG_Slideshow.image = UIImage(named: V_PicName)  //取得したインデックス番号の画像をImage Viewへセットする
+        //画像データ1枚目(インデックスが初期値のもの)を表示
+        GV_PicName = GL_ImageName[GV_Index]               //インデックス(初期値)の写真名を配列から変数に取得
+        IMG_Slideshow.image = UIImage(named: GV_PicName)  //取得した画像名をImage Viewへセットする
     }
 
+//--------------------------------------------------
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
-//--------------------------------------------------
-//  関数(Action)
-//--------------------------------------------------
-    //再生停止ボタンのAction作成 (0.03)
+//==================================================
+//  関数(Action) *画面遷移のAction除く
+//==================================================
+//--再生/停止ボタン押下時に実行されるAction---------------
     @IBAction func A_Push(_ sender: Any) {
-        
         //ステータスに応じてボタンの表示を切り替え & 画像を進めるためのタイマー操作
         if B_StartStop.currentTitle == "再生" {          //停止状態で再生ボタンを押下
             B_StartStop.setTitle("停止", for: .normal)       //ボタンタイトルを停止に
             B_Next.isEnabled = false                        //進むボタンを無効化
             B_Previous.isEnabled = false                    //戻るボタンを無効化
                                                             //2秒ごとに動く関数を呼び出し
-            self.V_Timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(F_TimerCallFunction), userInfo: nil, repeats: true)
+            self.GV_Timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(F_TimerCallFunction), userInfo: nil, repeats: true)
         } else {                                        //再生状態で停止ボタンを押下
             B_StartStop.setTitle("再生", for: .normal)       //ボタンタイトルを再生に
             B_Next.isEnabled = true                         //進むボタンを有効化
             B_Previous.isEnabled = true                     //戻るボタンを有効化
-            self.V_Timer.invalidate()                       //タイマーを破棄
+            self.GV_Timer.invalidate()                       //タイマーを破棄
         }
     }
     
-    //進むボタンのAction
+//--進むボタン押下時に実行されるAction-------------------
     @IBAction func A_Next(_ sender: Any) {
-        //1つ後の画像を表示
-        F_ShowNextImage()
+        F_ShowNextImage()            //1つ後の画像を表示
     }
     
-    //戻るボタンのAction
+//--戻るボタン押下時に実行されるAction-------------------
     @IBAction func A_Previous(_ sender: Any) {
-        //1つ前の画像を表示
-        F_ShowPreviousImage()
+        F_ShowPreviousImage()       //1つ前の画像を表示
     }
     
-    
-    //"ZoomView"から戻ってくるときのAction
-    @IBAction func A_Unwind(segue: UIStoryboardSegue) {
-    }    
-    
-//--------------------------------------------------
-//  関数(画面遷移)
-//--------------------------------------------------
-    // ZoomViewControllerの遷移時の値の受け渡し (0.35)
+//==================================================
+//  関数(画面遷移) *画面遷移のAction含む
+//==================================================
+//--View Controller => Zoom View Controller---------
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         //スライドショー再生時のタイマー破棄 (1.02)
         if B_StartStop.currentTitle == "停止" {
             B_StartStop.setTitle("再生", for: .normal)
-            self.V_Timer.invalidate()
+            self.GV_Timer.invalidate()
             B_Next.isEnabled = true          //(0.21)
             B_Previous.isEnabled = true      //(0.21)
         }
@@ -135,13 +127,17 @@ class ViewController: UIViewController {
         let L_ZoomViewController:ZoomViewController = segue.destination as! ZoomViewController
         
         // 遷移先のZoomViewControllerで宣言しているV_ImageNameに値を代入して渡す (0.35)
-        V_PicName = L_ImageName[V_Index]
-        L_ZoomViewController.V_ImageName = V_PicName
+        GV_PicName = GL_ImageName[GV_Index]
+        L_ZoomViewController.V_ImageName = GV_PicName
     }
     
-//--------------------------------------------------
+//--Zoom View Controller => View Controller---------
+    @IBAction func A_Unwind(segue: UIStoryboardSegue) {
+    }
+    
+//==================================================
 //  関数(内部呼び出し)
-//--------------------------------------------------
+//==================================================
 //--タイマー起動で起動される関数-------------------------
     func F_TimerCallFunction(timer: Timer) {
         // インデックスを繰り上げるの関数を呼び出し
@@ -152,40 +148,40 @@ class ViewController: UIViewController {
     func F_ShowNextImage() {
         
         //ローカル定数:配列の要素数からインデックスの最大値を取得
-        let LL_MaxIndex = L_ImageName.count - 1
+        let LL_MaxIndex = GL_ImageName.count - 1
         
         //インデックスを繰り上げる
-        if V_Index == LL_MaxIndex {      //最大の場合は0に戻す
-            V_Index = 0
+        if GV_Index == LL_MaxIndex {      //最大の場合は0に戻す
+            GV_Index = 0
         } else {
-            V_Index += 1                 //+1
+            GV_Index += 1                 //+1
         }
         
         //繰り上げたインデックスと同じインデックスの写真名を配列から変数に取得 (0.20)
-        V_PicName = L_ImageName[V_Index]
+        GV_PicName = GL_ImageName[GV_Index]
         
         //取得したインデックス番号の画像データを表示 (0.20)
-        IMG_Slideshow.image = UIImage(named: V_PicName) //繰り上げたインデックス番号の画像をImage Viewへセットする
+        IMG_Slideshow.image = UIImage(named: GV_PicName) //繰り上げたインデックス番号の画像をImage Viewへセットする
     }
 
 //--1つ前の画像を表示する関数----------------------------
     func F_ShowPreviousImage() {
         
         //ローカル定数:配列の要素数からインデックスの最大値を取得
-        let LL_MaxIndex = L_ImageName.count - 1
+        let LL_MaxIndex = GL_ImageName.count - 1
         
         //インデックスを繰り下げる (0.13)
-        if V_Index == 0 {
-            V_Index = LL_MaxIndex
+        if GV_Index == 0 {
+            GV_Index = LL_MaxIndex
         } else {
-            V_Index -= 1
+            GV_Index -= 1
         }
         
         //繰り上げたインデックスと同じインデックスの写真名を配列から変数に取得 (0.13)
-        V_PicName = L_ImageName[V_Index]
+        GV_PicName = GL_ImageName[GV_Index]
         
         //取得したインデックス番号の画像データを表示 (0.13)
-        IMG_Slideshow.image = UIImage(named: V_PicName) //繰り上げたインデックス番号の画像をImage Viewへセットする
+        IMG_Slideshow.image = UIImage(named: GV_PicName) //繰り上げたインデックス番号の画像をImage Viewへセットする
     }
 
 }
